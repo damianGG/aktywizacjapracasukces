319f0b240c0338b561d398cfedb237d8a19c8974
damianGG <damiangolon@gmail.com>
2025-07-14 23:39:21 +0200

    changed the subbjet in form

--- src/components/blocks/form/form.tsx
+++ src/components/blocks/form/form.tsx
@@ -40,7 +40,7 @@ export default function ContactForm() {
         const mailText = `Imię: ${values.name}\nEmail: ${values.email}\nNumer telefonu: ${values.phone}`;
         const response = await sendMail({
             email: values.email,
-            subject: 'Formularz ze strony czaskobiet.com.pl',
+            subject: 'Formularz ze strony aktywizacjapracasukces.pl',
             text: mailText,
         });
         if (response?.messageId) {
